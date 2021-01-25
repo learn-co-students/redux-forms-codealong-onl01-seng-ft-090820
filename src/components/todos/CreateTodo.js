df2f0a7463f5d3246124fd02bdb5c6bcf8c2fca1
= <=>
2021-01-25 10:45:55 -0700

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react'
 
-class CreateTodo extends Component {
-  render() {
-    return(
-      <div>
-        Create Todo Component
-      </div>
-    )
-  }
-}
+  class CreateTodo extends Component {
+    constructor() {
+      super();
+      this.state= {
+        text: ""
+      }
+    }
 
-export default CreateTodo;
+    handleChange = e => { 
+      this.setState({ text: e.target.value })
+    }
+
+    handleSubmit = e => { 
+        e.preventDefault()
+        this.props.addTodo(this.state) 
+    }
+    
+    render() {
+      return(
+        <div>
+          <label>Add Todo:</label>
+          <form onSubmit={this.handleSubmit}>
+            <input type="text"
+                  onChange={this.handleChange}
+                  value={this.state.text} />
+            <input type="Submit" />
+          </form>
+        </div>
+      )
+    }
+  }
+
+  const mapDispatchToProps = dispatch => ({
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  })
+
+
+export default connect(null, mapDispatchToProps)(CreateTodo)
